9cb6e247cef1d42af9558d8285961f38a6169760
Leandro Doctors <ldoctors@untref.edu.ar>
2021-03-22 16:42:37 -0300

    Diseñando la Clase: Paso 2 (Definición de Clase)
    Resolver este paso requiere decidir de qué se va a encargar la clase, y
    qué información va a necesitar para ello.
    
    - Agregamos las responsabilidades que queremos asignarle a nuestra clase
    como un comentario sobre su encabezado.
    - Puesto que en este caso el que haya una clase es sólo una exigencia
    del lenguaje (Java no permite simplemente crear métodos que no
    pertenezcan a una clase), por ahora vamos a evitar ocuparnos del
    constructor.

--- src/ar/edu/untref/ayp2/FizzBuzz.java
+++ src/ar/edu/untref/ayp2/FizzBuzz.java
@@ -1,5 +1,10 @@
 package ar.edu.untref.ayp2;
 
+/**
+ * Responsabilidades: Sólo necesitamos esta clase para poder escribir nuestro
+ * método dentro de ella :-)
+ *
+ */
 public class FizzBuzz {
 
 }
