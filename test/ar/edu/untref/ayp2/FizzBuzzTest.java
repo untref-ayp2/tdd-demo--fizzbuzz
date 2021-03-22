4570b69a098f7fbacc437362d7846f2e1e383e8d
Leandro Doctors <ldoctors@untref.edu.ar>
2021-03-22 17:26:39 -0300

    Diseñando la Clase: Paso 3 (Ejemplos Funcionales): FALLA
    En este paso definimos cómo queremos que nuestra clase sea invocada en
    el futuro.

--- test/ar/edu/untref/ayp2/FizzBuzzTest.java
+++ test/ar/edu/untref/ayp2/FizzBuzzTest.java
@@ -1,5 +1,14 @@
 package ar.edu.untref.ayp2;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 public class FizzBuzzTest {
 
+	@Test
+	void deberiaPoderRepresentarElNumeroUno() {
+		FizzBuzz fb = new FizzBuzz();
+		assertThat(fb.fizzBuzz(1)).isEqualTo("1");
+	}
 }
