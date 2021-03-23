6de9d3ccb77842ef98ddefedecef301fb18ccb8d
Leandro Doctors <ldoctors@untref.edu.ar>
2021-03-22 23:17:34 -0300

    Diseñando la Función: Paso 6 (Definición): PASA (Iteración 5)
    Finalmente, agegamos el caso para números divisibles por 3 y 5.
    (Puesto que son coprimos - ninguno es múltiplo del otro, también
    podríamos haber considerado los divisibles por 15.)
    
    - generalizamos nuestro algoritmo para poder pasar todas las pruebas
    hasta el momento.
    
    - observamos que este último cambio en el código de la función, al igual
    que el anterior, es mínimo (simplemente agregamos un caso): esto quiere
    decir que la implementación de nuestro algoritmo se ha estabilizado.
    
    Ya cumplimos todos los casos pedidos: la implementación del método que
    invoca a este es trivial, y queda como ejercicio para les alumnes.
    
    Como conclusión general,
    Observamos que nuestro código se va generalizando cada vez más, mientras
    que las pruebas se van volviendo cada vez más específicas...
    
    (En problemas más grandes, notamos que, progresivamente, las clases de
    prueba son cada vez menos que las de producción - pero su código es cada
    vez más abarcativo...)

--- src/ar/edu/untref/ayp2/FizzBuzz.java
+++ src/ar/edu/untref/ayp2/FizzBuzz.java
@@ -14,7 +14,9 @@ public class FizzBuzz {
 	 */
 	public static String fizzBuzz(int numero) {
 
-		if (numero % 3 == 0) {
+		if ((numero % 3 == 0) && (numero % 5 == 0)) {
+			return "FizzBuzz";
+		} else if (numero % 3 == 0) {
 			return "Fizz";
 		} else if (numero % 5 == 0) {
 			return "Buzz";
